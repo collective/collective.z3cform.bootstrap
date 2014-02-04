724c127f6c36260aa026255af8e1cf47cc66c915
Gaudenz Steinlin <gaudenz.steinlin@cirrax.com>
2014-02-04 15:23:18 +0100

    Initial package setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='collective.z3cform.bootstrap',
+      version=version,
+      description="Twitter Bootstrap widgets and templates for z3c.form",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Gaudenz Steinlin',
+      author_email='gaudenz.steinlin@cirrax.com',
+      url="'https://github.com/collective/collective.z3cform.bootstrap'",
+      license='GPL v3+',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.z3cform'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'z3c.form',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
